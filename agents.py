eaefd589c80d90273a9fca654c9fb533a3d600f3
Naman <adepnaman@gmail.com>
2025-01-10 17:50:23 +0530

    fixed api calling issue

--- agents.py
+++ agents.py
@@ -1,11 +1,10 @@
+from dotenv import load_dotenv
+load_dotenv()  
+
 from crewai import Agent
 from tools import yt_tool
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
 import os
+
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 os.environ["OPENAI_MODEL_NAME"]="gpt-4o-mini"
 
